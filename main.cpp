9cbb6319bfb781855f5cd8e8ae820f7317a576e9
Nikita Grigorev <pettyaelse@gmail.com>
2023-05-21 14:53:19 +0700

    1

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 #include <windows.h>
 #include <tchar.h>
+#include <fstream>
+#include <string>
+
+template<class T>
+void showMessage(T value)
+{
+    showMessage(std::to_string(value));
+}
+
+void showMessage(const std::string &text)
+{
+    MessageBox(nullptr, text.c_str(), nullptr, 0);
+}
+
 
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 TCHAR WinName[] = "MainFrame";
@@ -10,6 +24,21 @@ int APIENTRY _tWinMain(HINSTANCE instance,
                        LPTSTR cmd_line,
                        int window_mode)
 {
-    MessageBox(NULL, "Hello chel!", "Hello", 1);
+    std::ofstream file("logs.txt");
+
+    MSG message{};
+    int result = 0;
+    while ((result = GetMessage(&message, NULL, 0, 0)) != 0)
+    {
+        if (result == -1)
+            return 3;
+
+        showMessage(result);
+
+        TranslateMessage(&message);
+        DispatchMessage(&message);
+    }
+
+
     return 0;
 }
